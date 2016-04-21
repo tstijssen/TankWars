e43a2c0a30a653246d4c932e6ef3a66b64122e09
jstone123 <josh-stone123@hotmail.co.uk>
2016-04-21 18:36:58 +0100

    changed key bind number decalration
    Removed = 0. This could have caused an error when closing and re-opening the program. The error could lead to keys being assigned the wrong control.

--- TypeDefs.h
+++ TypeDefs.h
@@ -9,7 +9,7 @@ struct gameControls
 {
 	string control;
 	tle::EKeyCode keyBind;
-	int keyBindNumber = 0;//This variable will be used to write to the control file. This number is casted to an EKeyCode to determine what key to use for each control/action.
+	int keyBindNumber;//This variable will be used to write to the control file. This number is casted to an EKeyCode to determine what key to use for each control/action.
 };
 
 struct sBuilding
